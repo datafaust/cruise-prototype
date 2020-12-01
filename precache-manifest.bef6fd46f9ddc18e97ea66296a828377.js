2221e20a81edf88fc14068efb2a6dd2e2f5b5eeb
mac <mac@macs-MBP.fios-router.home>
2020-12-01 15:37:57 -0500

    Updates

---
+++ precache-manifest.bef6fd46f9ddc18e97ea66296a828377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96dd2f6e40df789be8185bde6f9bccea",
+    "url": "/cruise-prototype/index.html"
+  },
+  {
+    "revision": "3708bbf5dcd2d1a120ba",
+    "url": "/cruise-prototype/static/css/main.fb9d1ad3.chunk.css"
+  },
+  {
+    "revision": "3cdc830a57ca3a4b2cdb",
+    "url": "/cruise-prototype/static/js/2.eae5fb07.chunk.js"
+  },
+  {
+    "revision": "3708bbf5dcd2d1a120ba",
+    "url": "/cruise-prototype/static/js/main.6efb6642.chunk.js"
+  },
+  {
+    "revision": "9f8b59a84ff53798acaf",
+    "url": "/cruise-prototype/static/js/runtime-main.19479d00.js"
+  }
+]);
